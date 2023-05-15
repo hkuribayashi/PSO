8c3d7a1b882c0d1563aed50d47dc8f3682777d0d
hkuribayashi <hkuribayashi@gmail.com>
2023-05-15 17:08:07 -0300

    Inclusão de Nova Função

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def function_1(x):
     return y
 
 
+def function_2(x):
+    x = round(x, 2)
+    y = -1.0 * (x ** 2.0) + 2.0 * x + 3.0
+    return y
+
+
 pso = PSO(function_1, 200)
 pso.run(n_steps=100)
 
